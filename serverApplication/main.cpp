65cf21a7c5544050b2ef5c7e8c179d7265970093
Hasoo <vaxzeen@hotmail.com>
2018-06-14 01:49:43 +0900

    loadConfig

--- serverApplication/main.cpp
+++ serverApplication/main.cpp
@@ -9,16 +9,22 @@ using Poco::Util::Application;
 class MyApp : public ServerApplication {
 protected:
   void initialize(Application &application) {
-    this->loadConfiguration();
+    this->loadConfiguration(
+        "/root/project/ex_poco/serverApplication/test.json");
+    this->loadConfiguration(
+        "/root/project/ex_poco/serverApplication/test2.json");
     ServerApplication::initialize(application);
   }
 
   void uninitialize() { ServerApplication::uninitialize(); }
 
   int main(const std::vector<std::string> &arguments) {
-    if (config().getBool("application.runAsDaemon", true)) {
+    if (config().getBool("application.runAsDaemon", false)) {
+      std::cout << "Hello My Daemon App!!" << std::endl;
+    } else {
       std::cout << "Hello My App!!" << std::endl;
-      Poco::Thread::sleep(1000);
+      std::cout << config().getString("test.prop4") << std::endl;
+      std::cout << config().getString("test2.prop4") << std::endl;
       waitForTerminationRequest();
     }
     return EXIT_OK;
