d70bfb4e9233cac4ce5a685221352da68e572ad7
Hasoo <vaxzeen@hotmail.com>
2018-08-17 01:27:33 +0900

    root logger

--- asyncLogger/main.cpp
+++ asyncLogger/main.cpp
@@ -20,6 +20,13 @@ private:
   int _num;
   std::string _name;
 
+  Poco::FastMutex _mutex;
+
+  int getPlusNum() {
+    Poco::ScopedLock lock(this->_mutex);
+    return ++this->_num;
+  }
+
 public:
   MassiveLogger() : _num(0), _name("") {}
   MassiveLogger(const std::string &name) : _num(0), _name(name) {}
@@ -46,54 +53,59 @@ private:
   void uninitialize() { Poco::Util::Application::uninitialize(); }
 
   int main(const std::vector<std::string> &arguments) {
-    try {
-      std::string logPath("./"), logFilename("massive.log");
-      Poco::FileChannel *pFileChannel = new Poco::FileChannel;
-      pFileChannel->setProperty("rotation", "100 M");
-      pFileChannel->setProperty("archive", "timestamp");
-      pFileChannel->setProperty("path",
-                                Poco ::format("%s/%s", logPath, logFilename));
+    std::string logPath("./log"), logFilename("massive.log");
+    Poco::FileChannel *pFileChannel = new Poco::FileChannel;
+    pFileChannel->setProperty("rotation", "100 M");
+    pFileChannel->setProperty("archive", "timestamp");
+    pFileChannel->setProperty("path",
+                              Poco::format("%s/%s", logPath, logFilename));
 
-      Poco::AsyncChannel *pAsyncChannel = new Poco::AsyncChannel(pFileChannel);
+    Poco::AsyncChannel *pAsyncChannel = new Poco::AsyncChannel(pFileChannel);
 
-      Poco::Logger::create(
-          "async_logger",
-          new Poco::FormattingChannel(
-              new Poco::PatternFormatter("%L%H:%M:%S.%i [%P][%T] %p %t"),
-              pAsyncChannel),
-          Poco::Message::PRIO_TRACE);
+    Poco::Logger::create(
+        "async_logger",
+        new Poco::FormattingChannel(
+            new Poco::PatternFormatter("%L%H:%M:%S.%i [%P][%T] %p %t"),
+            pAsyncChannel),
+        Poco::Message::PRIO_TRACE);
 
-      /*
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker1");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker2");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker3");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker4");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker5");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker6");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker7");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker8");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker9");
-          Poco::ThreadPool::defaultPool().start(massiveLogger, "worker10");
+    Poco::Logger &logger = Poco::Logger::root();
+    logger.setLevel(Poco::Message::PRIO_TRACE);
+    logger.setChannel(new Poco::FormattingChannel(
+        new Poco::PatternFormatter("%L%H:%M:%S.%i [%P] %t"),
+        new Poco::ConsoleChannel));
+    /*
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker1");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker2");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker3");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker4");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker5");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker6");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker7");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker8");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker9");
+        Poco::ThreadPool::defaultPool().start(massiveLogger, "worker10");
 
-          Poco::ThreadPool::defaultPool().joinAll();
-      */
-      auto p = std::make_shared<MassiveLogger>("hasoo");
+        Poco::ThreadPool::defaultPool().joinAll();
+    */
+    auto p = std::make_shared<MassiveLogger>("hasoo");
 
-      Poco::ThreadPool *threadPool = new Poco::ThreadPool(2, 5);
-      while (true) {
-        try {
-          std::cout << "used count:" << threadPool->used() << std::endl;
-          while (true) {
-            int n = 0;
-            threadPool->start(*p, Poco::format("worker%d", ++n));
-            Poco::Thread::sleep(100);
-          }
-        } catch (Poco::NoThreadAvailableException &ex) {
-          std::cerr << ex.what() << std::endl;
-        }
+    const uint32_t thMinCount = 2;
+    const uint32_t thMaxCount = 10;
+    Poco::ThreadPool *threadPool = new Poco::ThreadPool(thMinCount, thMaxCount);
+    for (int n = 0; n < 1000; ++n) {
+      logger.trace(Poco::format("used count:%d", threadPool->used()));
+
+      if (thMaxCount == threadPool->used()) {
+        Poco::Thread::sleep(1000);
+        continue;
       }
-    } catch (Poco::Exception &ex) {
-      std::cerr << ex.what() << std::endl;
+
+      threadPool->start(*p, Poco::format("worker%d", n));
+      //      try {
+      //      } catch (Poco::NoThreadAvailableException &ex) {
+      //        logger.critical(ex.what());
+      //      }
     }
 
     return EXIT_OK;
--- dns/main.cpp
+++ dns/main.cpp
@@ -1,4 +1,8 @@
-#include "Poco/Net/DNS.h"
+#include <Poco/ConsoleChannel.h>
+#include <Poco/FormattingChannel.h>
+#include <Poco/Logger.h>
+#include <Poco/Net/DNS.h>
+#include <Poco/PatternFormatter.h>
 #include <iostream>
 
 using Poco::Net::DNS;
@@ -9,17 +13,23 @@ int main(int argc, char *argv[]) {
 
   const HostEntry &entry = DNS::hostByName("www.appinf.com");
 
-  std::cout << "Canonical Name: " << entry.name() << std::endl;
+  Poco::Logger &logger = Poco::Logger::root();
+  logger.setLevel(Poco::Message::PRIO_TRACE);
+  logger.setChannel(new Poco::FormattingChannel(
+      new Poco::PatternFormatter("%L%H:%M:%S.%i [%P] %t"),
+      new Poco::ConsoleChannel));
+
+  logger.trace(Poco::format("Canonical Name: %s", entry.name()));
 
   const HostEntry::AliasList &aliases = entry.aliases();
 
   for (auto it = aliases.begin(); it != aliases.end(); ++it)
-    std::cout << "Alias: " << *it << std::endl;
+    logger.trace(Poco::format("Alias: %s", *it));
 
   const HostEntry::AddressList &addrs = entry.addresses();
 
   for (auto it = addrs.begin(); it != addrs.end(); ++it)
-    std::cout << "Address: " << it->toString() << std::endl;
+    logger.trace(Poco::format("Address: %s", it->toString()));
 
   return 0;
 }
