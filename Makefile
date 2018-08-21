161169b70a57acd0bd2bea244f4fc0cbcac2510b
Hasoo <vaxzeen@hotmail.com>
2018-08-21 16:37:17 +0900

    config and errorhandler

--- Makefile
+++ Makefile
@@ -2,7 +2,11 @@
 
 clean:
 	cd ./application && make clean
-	cd ./config && make clean
+	cd ./asyncLogger && make clean
+	cd ./configuration && make clean
 	cd ./dns && make clean
+	cd ./errorThread && make clean
+	cd ./logger && make clean
+	cd ./loggingConfigurator && make clean
 	cd ./serverApplication && make clean
 	cd ./thread && make clean
--- serverApplication/main.cpp
+++ serverApplication/main.cpp
@@ -1,31 +1,38 @@
+#include <Poco/ConsoleChannel.h>
+#include <Poco/FormattingChannel.h>
 #include <Poco/Foundation.h>
+#include <Poco/PatternFormatter.h>
 #include <Poco/Util/ServerApplication.h>
 
 #include <iostream>
 
-using Poco::Util::ServerApplication;
 using Poco::Util::Application;
+using Poco::Util::ServerApplication;
 
 class MyApp : public ServerApplication {
 protected:
   void initialize(Application &application) {
-    this->loadConfiguration(
-        "/root/project/ex_poco/serverApplication/test.json");
-    this->loadConfiguration(
-        "/root/project/ex_poco/serverApplication/test2.json");
+    Poco::Logger &logger = this->logger();
+    logger.setChannel(new Poco::FormattingChannel(
+        new Poco::PatternFormatter("%L%H:%M:%S.%i [%P][%I] %p %t"),
+        new Poco::ConsoleChannel));
+    logger.setLevel("trace");
+
+    loadConfiguration();
     ServerApplication::initialize(application);
   }
 
   void uninitialize() { ServerApplication::uninitialize(); }
 
   int main(const std::vector<std::string> &arguments) {
+    Poco::Logger &logger = this->logger();
+
     if (config().getBool("application.runAsDaemon", false)) {
-      std::cout << "Hello My Daemon App!!" << std::endl;
+      logger.trace("Hello My Daemon App!!");
     } else {
-      std::cout << "Hello My App!!" << std::endl;
-      std::cout << config().getString("test.prop4") << std::endl;
-      std::cout << config().getString("test2.prop4") << std::endl;
-      waitForTerminationRequest();
+      logger.trace("Hello My App!!");
+      logger.trace(
+          Poco::format("config->%s", config().getString("config.prop4")));
     }
     return EXIT_OK;
   }
